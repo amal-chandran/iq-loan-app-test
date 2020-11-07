ba1e789aac78a83ec8a92ec3a67b4ba5b29e21df
Amal Chandran <amalchandrandev@gmail.com>
2020-11-08 01:00:25 +0530

    Init App 🎉

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  prefix: 't-',
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
